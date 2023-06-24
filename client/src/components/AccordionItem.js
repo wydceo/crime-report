3144833d2e9e683b9115c2a3554b68b35add339a
wangy <wydceo@msn.com>
2023-06-24 17:54:28 +1000

    1. Handle Toggle function for each accordion items. It can open multiple panels active. 2. Adjust css colour and size.

--- client/src/components/AccordionItem.js
+++ client/src/components/AccordionItem.js
@@ -1,12 +1,30 @@
-const AccordionItem = ({ record }) => {
+import { useRef } from "react";
+import { useState } from "react";
+
+const AccordionItem = ({ record, onToggle, active }) => {
+  const [clicked, setClicked] = useState(false);
+  const contentEl = useRef();
   const { id,  reported_date} = record;
+
+  const handleToggle = () => {
+    setClicked((prev) => !prev);
+  };
+
   return (
-    <li className="accordion_item">
-      <button className="button">
+    <li className={`accordion_item ${clicked ? "active" : ""}`}>
+      <button className="button" onClick={handleToggle}>
         {id}
-        <span className="control">—</span>
+        <span className="control">{clicked ? "-" : "+"}</span>
       </button>
-      <div className="record_wrapper">
+      <div
+        ref={contentEl}
+        className="record_wrapper"
+        style={
+          clicked
+            ? { height: contentEl.current.scrollHeight }
+            : { height: "0px" }
+        }
+      >
         <div className="record">{reported_date}</div>
       </div>
     </li>
