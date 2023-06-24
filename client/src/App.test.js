c18d4e64c7e19ca4abcf952e31ea8b48a3b626de
wangy <wydceo@msn.com>
2023-06-24 15:01:21 +1000

    Add Header.js component

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,5 +4,5 @@ import App from './App';
 
 it('renders welcome message', () => {
   render(<App />);
-  expect(screen.getByText('Hello World')).toBeInTheDocument();
+  expect(screen.getByText('Crime Report Accordion')).toBeInTheDocument();
 });
---
+++ client/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <h1 className="heading">
+      Crime Report Accordion
+    </h1>
+  )
+}
+
+export default Header;
